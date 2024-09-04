17e0ebd1b5d659710236d68a551fddcaaa1566ec
Param Kumar Singh <138215972+ParamSingh05@users.noreply.github.com>
2024-09-04 21:19:47 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+#print(len(input("Enter your name: ")))
+
+username = input("What is your name?\n")
+length = len(username)
+print("The length of your name is: ", length)
+
---
+++ Day1_Project.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+nameOfCity = input("What's the name of the city you grew up in?\n")
+nameOfPet = input("What's your pet's name?\n")
+print("Your band name could be", nameOfCity + " " + nameOfPet)
